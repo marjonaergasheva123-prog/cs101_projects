6824bf5a158e0163e3fd72cd728d25fdaf21ea43
marjonaergasheva123-prog <marjonaergasheva123@gmail.com>
2025-10-08 12:57:55 +0500

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,7 @@
+r= int(input("Enter your radius: "))
+pi = 3.14159
+Area = pi * r**2 
+Circumference = 2 *pi *r
+
+print(f"Area: {Area:.2f}") 
+print(f"Circumference: {Circumference:.2f}")
---
+++ temperature.py
@@ -0,0 +1,8 @@
+Celsius=  float(input("Enter your temperature (celsius): "))
+
+Fahrenheit = (Celsius * 9/5) + 32
+Kelvin = Celsius + 273.15
+
+print(f"Celcius temperature: {Celsius:.2f}")
+print(f"Fahreheit temperature : {Fahrenheit:.2f}")
+print(f" Kelvin temperature : {Kelvin:.2f}")
